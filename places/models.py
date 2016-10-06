a9fb8c37b004bd7e4ce2f7dc2a9a2ed544ca3a1e
LopsanAMO <makiki789@gmail.com>
2016-10-05 23:12:39 -0500

    crendo los modelos para los pishis lugares

--- places/models.py
+++ places/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Lugares(models.Model):
+    nombre = models.CharField(blank=False, null=False, max_length=100)
+    descripcion = models.CharField(blank=False, null=False, max_length=100)
+    latitud = models.CharField(blank=False, null=False, max_length=100)
+    longitud = models.CharField(blank=False, null=False, max_length=100)
+    lugares_cercanos = models.CharField(blank=False, null=False, max_length=100)
+    historio_lugar = models.CharField(blank=False, null=False, max_length=100)
+    horario = models.CharField(blank=False, null=False, max_length=100)
+    costo_entrada = models.CharField(blank=False, null=False, max_length=100)
+
+class Foto(models.Model):
+    lugar = models.OneToOneField(Lugares, blank=False, null=False)
+    foto = models.ImageField(blank=False, null=False)
