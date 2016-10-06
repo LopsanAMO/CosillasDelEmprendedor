9037f674c6dabe159f863969dadbde1e9a357739
cognatus <diegojmartinezzm@gmail.com>
2016-10-06 01:41:09 -0500

    Preparado el angular para recibir lo del mapa y así

---
+++ nodeStuff/public/javascripts/controller.js
@@ -0,0 +1,40 @@
+var aplicacion = angular.module('aplicacion', []);
+
+aplicacion.controller('mapa', function($scope, $http, $document) {
+
+	$scope.nombre;
+	$scope.descripcion;
+	$scope.latitud;
+  $scope.longitud;
+  $scope.lugares_cercanos;
+	$scope.historio_lugar;
+  $scope.horario;
+  $scope.costo_entrada;
+	$scope.fotos;
+
+	$scope.init = function() {
+		$scope.cargaApi();
+	}
+
+	$scope.cargaApi = function(){
+		$http({
+			method: 'GET',
+			url: '/lugares/lugares/?format=json'
+		}).
+		success(function(data) {
+			$scope.nombre = data.nombre;
+			$scope.descripcion = data.descripcion;
+			$scope.latitud = data.latitud;
+		  $scope.longitud = data.longitud;
+		  $scope.lugares_cercanos = data.lugares_cercanos;
+			$scope.historio_lugar = data.historio_lugar;
+		  $scope.horario = data.horario;
+		  $scope.costo_entrada = data.costo_entrada;
+			$scope.fotos = data.fotos;
+		}).
+		error(function() {
+			alert('Error obteniendo lo del mapa');
+		});
+	};
+
+});
